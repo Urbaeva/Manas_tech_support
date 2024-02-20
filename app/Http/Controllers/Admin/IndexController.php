0f6674ddf018785fe902371dc218adfd91830b49
Urbaeva <burulaiurbaeva7@gmail.com>
2024-02-20 21:55:32 +0600

    added statistic

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -10,5 +11,23 @@ class IndexController extends Controller
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function statistic()
+    {
+        $videos = Video::orderBy('views', 'desc')->get();
+        return view('admin.statistic.index', ['videos' => $videos]);
+    }
+
+    public function getVideoStatistics(Video $video)
+    {
+        $video = Video::findOrFail($video->id);
+
+        $data = [
+            'views' => $video->views,
+            // Add more statistics fields as needed
+        ];
+
+        return response()->json($data);
     }
 }
